0a8109ddc49c0d89adf03ae0dbeb9309ff6008c2
oovaa <omarvx211@gmail.com>
2025-01-05 12:36:14 +0200

    add Tailwind CSS and PostCSS configuration; refactor App and Home components for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import Circle from "./Circle";
-import Home from "./components/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Upload from "./components/Upload";
-import Chat from "./components/Chat";
+import Circle from './Circle'
+import Home from './components/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Upload from './components/Upload'
+import Chat from './components/Chat'
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/" Component={Home} />
-          <Route path="/upload" Component={Upload} />
-          <Route path="/chat" Component={Chat} />
-        </Routes>
-        <Circle />
-      </div>
-    </Router>
-  );
+    <div className='min-h-screen bg-gradient-to-t from-green-900 via-black to-green-950'>
+      <Router>
+        <div className=''>
+          <Routes>
+            <Route path='/' Component={Home} />
+            <Route path='/upload' Component={Upload} />
+            <Route path='/chat' Component={Chat} />
+          </Routes>
+          <Circle />
+        </div>
+      </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
