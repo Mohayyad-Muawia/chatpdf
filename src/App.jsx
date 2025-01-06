1962f8d70f5b10f2710bfbee8a256b3a06ef52cd
oovaa <omarvx211@gmail.com>
2025-01-06 11:09:14 +0200

    refactor NavBar component: remove unnecessary whitespace and update styling for nav items

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import NavBar from './components/NavBar'
 function App() {
   return (
     <div className='min-h-screen bg-gradient-to-t to-black from-green-950'>
-      <NavBar> </NavBar>
+      <NavBar />
       <Router>
         <div className=''>
           <Routes>
