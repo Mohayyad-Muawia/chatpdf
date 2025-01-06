d6e214dd61ea362d2076bda8b7753aeb298baa7f
oovaa <omarvx211@gmail.com>
2025-01-06 16:45:31 +0200

    enhance Upload component with file selection and upload functionality

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Upload = () => {
   const [isDragging, setIsDragging] = useState(false)
+  const navigate = useNavigate()
 
   const handleDragOver = (e) => {
     e.preventDefault()
@@ -12,12 +14,66 @@ const Upload = () => {
     setIsDragging(false)
   }
 
-  const handleDrop = (e) => {
+  const handleDrop = async (e) => {
     e.preventDefault()
     setIsDragging(false)
     const files = e.dataTransfer.files
-    // Handle the dropped files
-    console.log(files)
+
+    if (files.length > 0) {
+      console.log(files[0])
+
+      const formData = new FormData()
+      formData.append('file', files[0])
+
+      try {
+        const response = await fetch(
+          'https://chatpdf-9g4j.onrender.com/api/v1/upload',
+          {
+            method: 'POST',
+            body: formData,
+          }
+        )
+
+        if (response.ok) {
+          console.log('File uploaded successfully')
+          navigate('/chat')
+        } else {
+          console.error('File upload failed')
+        }
+      } catch (error) {
+        console.error('Error uploading file:', error)
+      }
+    }
+  }
+
+  const handleFileSelect = async (e) => {
+    const files = e.target.files
+
+    if (files.length > 0) {
+      console.log(files[0])
+
+      const formData = new FormData()
+      formData.append('file', files[0])
+
+      try {
+        const response = await fetch(
+          'https://chatpdf-9g4j.onrender.com/api/v1/upload',
+          {
+            method: 'POST',
+            body: formData,
+          }
+        )
+
+        if (response.ok) {
+          console.log('File uploaded successfully')
+          navigate('/chat')
+        } else {
+          console.error('File upload failed')
+        }
+      } catch (error) {
+        console.error('Error uploading file:', error)
+      }
+    }
   }
 
   return (
@@ -25,7 +81,7 @@ const Upload = () => {
       <div
         className={`border-4 p-8 rounded-md w-full max-w-4xl flex transition justify-center items-center ${
           isDragging
-            ? 'border-gray-800 bg-green-100/20 '
+            ? 'border-gray-800 bg-green-100/20'
             : 'border-gray-500/20 bg-white/10 backdrop-blur-sm'
         }`}
         style={{ minHeight: '80vh' }}
@@ -33,10 +89,16 @@ const Upload = () => {
         onDragLeave={handleDragLeave}
         onDrop={handleDrop}
       >
-        <input type="file" className="hidden" id="fileUpload" />
+        <input
+          type="file"
+          className="hidden"
+          id="fileUpload"
+          onChange={handleFileSelect}
+          accept="application/pdf, image/*"
+        />
         <label
           htmlFor="fileUpload"
-          className="bg-transparent animate-bounce cursor-pointer text-gray-400/90 text-lg text-center"
+          className="bg-transparent cursor-pointer animate-bounce text-gray-400/90 text-lg text-center"
         >
           {isDragging
             ? 'Drop files here'
