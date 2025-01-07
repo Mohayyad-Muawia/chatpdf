f857d0b45cb5d17318f3b87d1803ebcc913c0c13
mohayyd-muawia <mohayyad2.0@gmail.com>
2025-01-07 18:51:12 +0200

    add loading in upload and chat

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,14 @@
+import React from 'react'
+import './loading.css'
+
 const Loading = () => {
   return (
     <div className="loading">
-      <b>Loading...</b>
+      <span className="one"> </span>
+      <span className="two"> </span>
+      <span className="three"> </span>
     </div>
-  );
-};
+  )
+}
 
-export default Loading;
+export default Loading
--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,90 +1,94 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Loading from './Loading'
 
 const Upload = () => {
-  const [isDragging, setIsDragging] = useState(false);
-  const navigate = useNavigate();
+  const [isDragging, setIsDragging] = useState(false)
+  const [loading, setIsLoading] = useState(false)
+  const navigate = useNavigate()
 
   const handleDragOver = (e) => {
-    e.preventDefault();
-    setIsDragging(true);
-  };
+    e.preventDefault()
+    setIsDragging(true)
+  }
 
   const handleDragLeave = () => {
-    setIsDragging(false);
-  };
+    setIsDragging(false)
+  }
 
   const handleDrop = async (e) => {
-    e.preventDefault();
-    setIsDragging(false);
-    const files = e.dataTransfer.files;
+    e.preventDefault()
+    setIsDragging(false)
+    setIsLoading(true)
+    const files = e.dataTransfer.files
 
     if (files.length > 0) {
-      console.log(files[0]);
+      console.log(files[0])
 
-      const formData = new FormData();
-      formData.append("file", files[0]);
+      const formData = new FormData()
+      formData.append('file', files[0])
 
       try {
         const response = await fetch(
-          "https://chatpdf-9g4j.onrender.com/api/v1/upload",
+          'https://chatpdf-9g4j.onrender.com/api/v1/upload',
           {
-            method: "POST",
+            method: 'POST',
             body: formData,
           }
-        );
+        )
 
         if (response.ok) {
-          console.log("File uploaded successfully");
-          navigate("/chat");
+          console.log('File uploaded successfully')
+          setIsLoading(false)
+          navigate('/chat')
         } else {
-          console.error("File upload failed");
+          console.error('File upload failed')
         }
       } catch (error) {
-        console.error("Error uploading file:", error);
+        console.error('Error uploading file:', error)
       }
     }
-  };
+  }
 
   const handleFileSelect = async (e) => {
-    const files = e.target.files;
+    const files = e.target.files
 
     if (files.length > 0) {
-      console.log(files[0]);
+      console.log(files[0])
 
-      const formData = new FormData();
-      formData.append("file", files[0]);
+      const formData = new FormData()
+      formData.append('file', files[0])
 
       try {
         const response = await fetch(
-          "https://chatpdf-9g4j.onrender.com/api/v1/upload",
+          'https://chatpdf-9g4j.onrender.com/api/v1/upload',
           {
-            method: "POST",
+            method: 'POST',
             body: formData,
           }
-        );
+        )
 
         if (response.ok) {
-          console.log("File uploaded successfully");
-          navigate("/chat");
+          console.log('File uploaded successfully')
+          navigate('/chat')
         } else {
-          console.error("File upload failed");
+          console.error('File upload failed')
         }
       } catch (error) {
-        console.error("Error uploading file:", error);
+        console.error('Error uploading file:', error)
       }
     }
-  };
+  }
 
   return (
-    <div className="upload flex justify-center">
+    <div className="upload flex justify-center relative">
       <div
         className={`border-4 p-8 rounded-md w-full max-w-4xl flex transition justify-center items-center ${
           isDragging
-            ? "border-gray-800 bg-green-100/20"
-            : "border-gray-500/20 bg-white/10 backdrop-blur-sm"
+            ? 'border-gray-800 bg-green-100/20'
+            : 'border-gray-500/20 bg-white/10 backdrop-blur-sm'
         }`}
-        style={{ minHeight: "80vh" }}
+        style={{ minHeight: '80vh' }}
         onDragOver={handleDragOver}
         onDragLeave={handleDragLeave}
         onDrop={handleDrop}
@@ -101,11 +105,26 @@ const Upload = () => {
           className="bg-transparent cursor-pointer animate-bounce text-gray-400/90 text-lg text-center"
         >
           {isDragging
-            ? "Drop files here"
-            : "Drag & drop files here or click to upload"}
+            ? 'Drop files here'
+            : loading
+              ? 'Loading your file, please wait'
+              : 'Drag & drop files here or click to upload'}
         </label>
       </div>
+      {loading && (
+        <div
+          style={{
+            position: 'absolute',
+            top: '55%',
+            left: '50%',
+            transform: 'tranlate(-50%, -50%)',
+            scale: 2,
+          }}
+        >
+          <Loading />
+        </div>
+      )}
     </div>
-  );
-};
+  )
+}
 export default Upload
