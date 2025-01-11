2647785ec15826f77939dcc2f25989357b82f896
mohayyd-muawia <mohayyad2.0@gmail.com>
2025-01-11 20:20:56 +0200

    add why and goal to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,17 +3,26 @@ const About = () => {
     <div className="about" style={{ height: 'calc(100vh - 64px)' }}>
       <div className="flex h-full items-center">
         <div className="flex-1 px-16">
-          <h1 className="font-bold text-4xl mb-5">About ChatPDF</h1>
-          <p className="opacity-50">
+          <h1 className="font-bold text-4xl">About ChatPDF</h1>
+          <p className="opacity-50 my-5">
             ChatPDF is your intelligent document assistant. Upload PDF, TXT,
             DOCX, or PPTX files and let the power of AI transform the way you
             read, analyze, and interact with your content. From quick summaries
             to in-depth answers, ChatPDF has you covered.
           </p>
+          <h1 className="font-bold text-4xl mt-8">Why ChatPDF</h1>
+          <p className="opacity-50 mt-5">
+            The inspiration behind our project, a chat PDF AI companion for
+            managing and interacting with PDF documents, stemmed from a
+            recognition of the challenges many individuals face when dealing
+            with digital documents. We observed that conventional methods of
+            organizing and navigating PDFs often lack efficiency and
+            user-friendliness.
+          </p>
         </div>
 
         <div className="flex-1 border-l px-16">
-          <h1 className="font-bold text-4xl mb-">How it works</h1>
+          <h1 className="font-bold text-4xl">How it works</h1>
           <ol className="mt-5 opacity-50 flex flex-col gap-3">
             <li>
               1. Chosoe the document you want ChatPDF to help you with, it must
@@ -25,6 +34,14 @@ const About = () => {
               anything.
             </li>
           </ol>
+          <h1 className="font-bold text-4xl mt-8">Our Goal</h1>
+          <p className="opacity-50 mt-5">
+            our goal is to empower individuals to effortlessly navigate, search,
+            annotate, and collaborate on PDF documents, revolutionizing the way
+            they interact with digital content. Through innovative technology
+            and a user-centric approach, we aspire to make PDF management more
+            accessible, efficient, and enjoyable for everyone.
+          </p>
         </div>
       </div>
 
