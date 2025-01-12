40447a150bbeaca14172998cf6fc701059fb700e
oovaa <omarvx211@gmail.com>
2025-01-12 11:59:29 +0200

    fix: correct typos and improve text formatting in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,18 +25,18 @@ const About = () => {
           <h1 className="font-bold text-4xl">How it works</h1>
           <ol className="mt-5 opacity-50 flex flex-col gap-3">
             <li>
-              1. Chosoe the document you want ChatPDF to help you with, it must
+              1. Choose the document you want ChatPDF to help you with, it must
               be PDF, TXT, DOCX, or PPTX.
             </li>
             <li>2. Upload your document to ChatPDF from the upload page.</li>
             <li>
-              3. Now you can enjoy and Chat about your document and ask for
+              3. Now you can enjoy and chat about your document and ask for
               anything.
             </li>
           </ol>
           <h1 className="font-bold text-4xl mt-8">Our Goal</h1>
           <p className="opacity-50 mt-5">
-            our goal is to empower individuals to effortlessly navigate, search,
+            Our goal is to empower individuals to effortlessly navigate, search,
             annotate, and collaborate on PDF documents, revolutionizing the way
             they interact with digital content. Through innovative technology
             and a user-centric approach, we aspire to make PDF management more
@@ -49,9 +49,9 @@ const About = () => {
         <h1 className="font-bold text-4xl mb-5 text-center">The Team</h1>
 
         <div className="members py-12 flex gap-12 flex-wrap">
-          <div className="card flex-1 min-w-60 rounded-2xl">
+          <div className="card flex-1 min-w-[250px] p-5 rounded-2xl shadow-lg">
             <img
-              className="max-w-full rounded-2xl"
+              className="w-full h-80 object-cover rounded-2xl"
               src="/assets/t1.jpg"
               alt=""
             />
@@ -62,7 +62,7 @@ const About = () => {
               technical studies, I'm also skilled in logo design, blending
               creativity with software expertise.
             </p>
-            <div className="links pt-6 flex justify-evenly -0">
+            <div className="links pt-6 flex justify-evenly">
               <a href="http://" target="_blank">
                 <img
                   src="/assets/x.svg"
@@ -87,9 +87,9 @@ const About = () => {
             </div>
           </div>
 
-          <div className="card flex-1 min-w-60">
+          <div className="card flex-1 min-w-[250px] p-5 rounded-2xl shadow-lg">
             <img
-              className="max-w-full rounded-2xl"
+              className="w-full h-80 object-cover rounded-2xl"
               src="/assets/mna.jpg"
               alt=""
             />
@@ -97,10 +97,9 @@ const About = () => {
             <p>
               ALX SE alumnus, ALX SE Africa student. 🌟 Mastermind behind the
               creation of intricate structures, chains, and bot functionalities.
-              🔗💡 full-stack dev with an insatiable hunger for tech and
-              AI
+              🔗💡 full-stack dev with an insatiable hunger for tech and AI
             </p>
-            <div className="links pt-6 flex justify-evenly -0">
+            <div className="links pt-6 flex justify-evenly">
               <a href="http://" target="_blank">
                 <img
                   src="/assets/x.svg"
@@ -125,9 +124,9 @@ const About = () => {
             </div>
           </div>
 
-          <div className="card flex-1 min-w-60">
+          <div className="card flex-1 min-w-[250px] p-5 rounded-2xl shadow-lg">
             <img
-              className="max-w-full rounded-2xl"
+              className="w-full h-80 object-cover rounded-2xl"
               src="/assets/t3.jpg"
               alt=""
             />
@@ -137,7 +136,7 @@ const About = () => {
               not coding, you’ll find me exploring new techs, tackling
               challenges, and bringing innovative projects to life :)
             </p>
-            <div className="links pt-6 flex justify-evenly -0">
+            <div className="links pt-6 flex justify-evenly">
               <a href="http://" target="_blank">
                 <img
                   src="/assets/x.svg"
