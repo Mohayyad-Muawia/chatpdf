3d5067279ab2295915fea1b06c34fa682736cf8b
oovaa <omarvx211@gmail.com>
2025-01-20 13:45:37 +0200

    add navigation functionality to logo in NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { FaBars } from 'react-icons/fa'
 
 const NavBar = () => {
   const location = useLocation()
   const [menuOpened, setMenuOpened] = useState(false)
+  const navigate = useNavigate()
 
   return (
     <header className="container mx-auto">
       <div className="relative text-white h-[70px] flex justify-between items-center">
-        <img className="max-h-full" src="/assets/logo.svg" alt="logo" />
-        <nav className={``}>
+        <img
+          className="max-h-full cursor-pointer"
+          onClick={() => navigate('/')}
+          src="/assets/logo.svg"
+          alt="logo"
+        />
+        <nav>
           <ul
             className={`${menuOpened ? 'scale-y-100' : 'scale-y-0'} md:scale-100 transition-transform origin-top max-md:absolute left-1/2 bottom-0 max-md:-translate-x-1/2 max-md:translate-y-full max-md:backdrop-blur max-md:w-full z-20 text-center md:flex justify-center md:gap-10 lg:gap-14`}
           >
